654cb464a73103db999db66b7023ae0eb46894ee
Shivam Gupta <shiva6588@gmail.com>
2023-11-06 05:39:58 +0530

    Created SignIn api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,10 @@
 import User from '../models/user.models.js';
 import bcryptjs from 'bcryptjs';
 import errorHandler from '../utils/error.js';
+import jwt from'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const signup= async(req,res,next)=>{
 
    const {username,email,password}=req.body;
@@ -13,4 +17,38 @@ export const signup= async(req,res,next)=>{
     {
       next(error);
     }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  try {
+    const validUser = await User.findOne({ email });
+
+    if (!validUser) {
+      const error = new Error('User not found');
+      error.status = 401;
+      return next(error);
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+
+    if (!validPassword) {
+      const error = new Error('Wrong password');
+      error.status = 402;
+      return next(error);
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+
+    res.cookie('access_token', token, { httpOnly: true })
+      .status(200)
+      .json({
+        rest,
+      });
+
+  } catch (error) {
+    next(error);
+  }
 };
