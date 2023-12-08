99b49fe5a83278953038a8144d81795425490427
Shivam Gupta <shiva6588@gmail.com>
2023-12-08 07:39:38 +0530

    Oauth Function

--- client/src/components/Oauth.jsx
+++ client/src/components/Oauth.jsx
@@ -2,7 +2,7 @@ import { GoogleAuthProvider } from 'firebase/auth';
 import { getAuth, signInWithPopup } from 'firebase/auth';
 import {app} from '../firebase';
 import {useDispatch} from 'react-redux';
-import { signInSuccess,signInFailure } from '../redux/user/userSlice';
+import { signInSuccess} from '../redux/user/userSlice';
 import {useNavigate} from 'react-router-dom';
 
 export default function Oauth() {
