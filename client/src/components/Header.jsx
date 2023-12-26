f7a9ba7950ea0808898be5d34894254e69e38641
Shivam Gupta <shiva6588@gmail.com>
2023-12-26 11:49:33 +0530

    Added Serach form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,9 +2,32 @@
 import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import {useSelector} from 'react-redux';
+import { useEffect, useState } from 'react';
+import {useNavigate} from 'react-router-dom';
 
-const Header = () => {
-  const {currentUser}=useSelector(state=>state.user); 
+  const Header = () => {
+    const navigate=useNavigate();
+    const {currentUser}=useSelector(state=>state.user); 
+    const [searchTerm, setSearchTerm] = useState('');
+
+    const hanldeFormSubmit=(e)=>{
+      e.preventDefault();
+      const urlParams= new URLSearchParams(window.location.search);
+      urlParams.set('searchTerm',searchTerm);
+      const searchQuery=urlParams.toString();
+      navigate(`/Search?${searchQuery}`);
+    }
+
+    useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get('searchTerm');
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+
+    }
+  }, [location.search]);
+
+  
   return (
     <header className="bg-gray-800 shadow-sm">
       <div className="flex justify-between items-center mx-w-6xl mx-auto p-3">
@@ -14,13 +37,15 @@ const Header = () => {
             <span className="text-blue-700">Estate</span>
           </h1>
         </Link>
-        <form className="bg-gray-600 rounded-full p-3 flex flex-direction-row items-center">
+        <form onSubmit={hanldeFormSubmit} className="bg-gray-600 rounded-full p-3 flex flex-direction-row items-center">
           <input
             type="text"
             placeholder="Search...."
+            value={searchTerm}
             className="w-24 sm:w-64 focus:outline-none bg-transparent text-gray-100"
+            onChange={(e)=>setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-gray-200 hover:text-blue-500 hover:text-xl cursor-pointer" />
+          <FaSearch type="submit" className="text-gray-200 hover:text-blue-500 hover:text-xl cursor-pointer" />
         </form>
         <ul className="flex gap-4 text-gray-300">
           <Link to="/">
@@ -33,9 +58,11 @@ const Header = () => {
               <li className="hover:underline cursor-pointer">Sign In</li>
             )}
           </Link>
+          {!currentUser &&(
           <Link to="/sign-up">
             <li className="hover:underline cursor-pointer">Sign Up</li>
-          </Link>
+          </Link>)
+          }
         </ul>
       </div>
     </header>
