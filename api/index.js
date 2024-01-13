a5e6bdc0c23de6e75e074dfaeb4b586f7ee6b308
Shivam Gupta <shiva6588@gmail.com>
2024-01-13 16:55:30 +0530

    Deploy Sample

--- api/index.js
+++ api/index.js
@@ -4,10 +4,12 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 import dotenv from 'dotenv';
 dotenv.config();
 
+const _dirname= path.resolve();
 
 const app= express();
 
@@ -27,6 +29,12 @@ app.use(cookieParser());
 app.use('/api/user', userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing',listingRouter);
+
+app.use(express.static(path.join(_dirname,'/client/dist')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(_dirname,'client','dist','index.html'));
+})
 
 app.use((err,req,res,next)=>{
     const statuscode=err.statusCode||500;
